730bb0d757906f98fadf11f3bf32ff4012a0eb22
Rafy <Rafy@DESKTOP-ADC4ST3>
2019-03-12 16:52:30 +0200

    After getting accepted

--- Flipping bits/Program.cs
+++ Flipping bits/Program.cs
@@ -8,8 +8,31 @@ namespace Flipping_bits
 {
     class Program
     {
+        static long flippingBits(long n)
+        {
+            
+            uint y = Convert.ToUInt32(n);
+            return ~y;
+
+        }
+
         static void Main(string[] args)
         {
+            //  TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine());
+
+                long result = flippingBits(n);
+
+                Console.WriteLine(result);
+            }
+
+            // textWriter.Flush();
+            // textWriter.Close();
         }
     }
 }
